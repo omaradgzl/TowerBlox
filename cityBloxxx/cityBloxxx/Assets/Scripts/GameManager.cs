35dcf0677a4ad586a36f0d2b139280b618085630
w0Lfg4ang <67344593+w0Lfg4ang@users.noreply.github.com>
2021-02-22 16:04:50 +0300

    base logic

---
+++ cityBloxxx/cityBloxxx/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class GameManager : MonoBehaviour
+{
+    private CubeSpawner[] spawners;
+    private CubeSpawner currentSpawner;
+    private int spawnerIndex;
+
+    private void Awake()
+    {
+        spawners = FindObjectsOfType<CubeSpawner>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (MovingCube.CurrentCube!=null)
+                MovingCube.CurrentCube.Stop();
+            spawnerIndex = spawnerIndex == 0 ? 1 : 0;
+            currentSpawner = spawners[spawnerIndex];
+            
+            currentSpawner.SpawnCube();
+        }
+    }
+}
---
+++ cityBloxxx/cityBloxxx/Assets/Scripts/MovingCube.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MovingCube : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+    public static MovingCube CurrentCube { get; private set; }
+    public static MovingCube LastCube { get; private set; }
+    public MoveDirection MoveDirection { get; set; }
+
+
+    private void OnEnable()
+    { 
+        
+        if (LastCube == null)
+        {
+            LastCube = GameObject.Find("Start").GetComponent<MovingCube>();
+        }
+
+        CurrentCube = this;
+        CurrentCube.GetComponent<Rigidbody>().useGravity = false;
+        
+    }
+
+    void Update()
+    {
+        if (MoveDirection==MoveDirection.Z)
+        {
+            transform.position += transform.forward * Time.deltaTime * moveSpeed;    
+        }else
+            transform.position += transform.right * Time.deltaTime * moveSpeed;
+    }
+
+    public void Stop()
+    {
+        moveSpeed = 0;
+        float hangover = GetHangover();
+        Debug.Log(hangover);
+        if (MoveDirection==MoveDirection.Z)
+            SetPosCubeOnZ(hangover);
+        else
+            SetPosCubeOnX(hangover);
+    }
+
+    private float GetHangover()
+    {
+        if (MoveDirection==MoveDirection.Z)
+            return transform.position.z - LastCube.transform.position.z;    
+        else
+            return transform.position.x - LastCube.transform.position.x;
+        
+    }
+
+    private void SetPosCubeOnX(float hangover)
+    {
+        if (Mathf.Abs(hangover)>0.6f)
+        { 
+            CurrentCube.GetComponent<Rigidbody>().useGravity = true;
+            /*LastCube = null;    DEATH
+            CurrentCube = null;
+            SceneManager.LoadScene(0);*/
+        }
+        else
+        { 
+            transform.position=new Vector3(transform.position.x,transform.position.y,transform.position.z);
+            Destroy(CurrentCube.GetComponent<Rigidbody>());
+            LastCube = this;
+        }
+        
+    }
+    
+    private void SetPosCubeOnZ(float hangover)
+    {
+        if (Mathf.Abs(hangover)>0.6f)
+        { 
+            CurrentCube.GetComponent<Rigidbody>().useGravity = true;
+            /*LastCube = null;    DEATH
+            CurrentCube = null;
+            sceneManager.LoadScene(0);*/
+        }
+        else
+        { 
+            transform.position=new Vector3(transform.position.x,transform.position.y,transform.position.z);
+            Destroy(CurrentCube.GetComponent<Rigidbody>());
+            LastCube = this;
+        }
+        
+    }
+
+    
+}
